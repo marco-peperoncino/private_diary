fc57f3c1496dda1b0dd190372b4a649656767fd2
marco-peperoncino <marco.peperoncino@gmail.com>
2020-07-08 11:41:41 +0900

    SECRET KEY対策

--- private_diary/private_diary/settings.py
+++ private_diary/private_diary/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+# シークレットキー対策
+# local_setting.py読み込み
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rb)fjof!%w5jsq018+_rd5cv#sbcc*eu6eme6c-$zz+-)lun%-'
+# SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
